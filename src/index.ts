71702759da43501837ba12554f7ee21c8f9b8c7e
Sam Schick <schicksw@protonmail.com>
2020-08-20 13:59:08 -0400

    added exports of new types

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import { Array } from './array'
 import { AsTypescript } from './domain'
 import { LogicalType } from './logicalType'
 import { Record, Field } from './record'
-import { String, Null, Boolean, Int, Long } from './primitives'
+import { String, Null, Boolean, Int, Long, Double } from './primitives'
 import { Union } from './union'
+import { Named } from './named'
 
 export {
     Array,
@@ -17,4 +18,6 @@ export {
     Field,
     String,
     Union,
+    Named,
+    Double
 }
